93ad3c6a8890281e70cd63f13d1f2ed984955592
Alex Lin <alexlin.dev@gmail.com>
2015-07-03 16:28:55 -0600

    changed to BrowserAction

---
+++ eventPage.js
@@ -0,0 +1,6 @@
+chrome.tabs.onUpdated.addListener(function(tabId, changeInfo, tab) {
+	if (tab.url.indexOf('.pbagalleries.com/') > -1) {
+		chrome.pageAction.setIcon({"tabId":tabId, "path":"icon.png"});
+		chrome.pageAction.show(tabId);
+	}
+});
